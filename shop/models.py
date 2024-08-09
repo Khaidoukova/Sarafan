09cc0e7e745ce33b50c17d309d15341aba6e2d20
Olga Khaidoukova <khaidoukova@inbox.ru>
2024-08-09 17:36:42 +0500

    Added cart and cart item models, serializers.

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils.text import slugify
+
+from users.models import User
 
 
 class Category(models.Model):
+    """ Модель категории """
     title = models.CharField(max_length=50, verbose_name='Категория')
     slug = models.SlugField(verbose_name='Слаг', unique=True)
     image = models.ImageField(upload_to='category/', verbose_name='Изображение', blank=True, null=True)
@@ -13,8 +17,15 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+    def save(self, *args, **kwargs):
+        ''' Сохранение поля slug по названию категории '''
+
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
 
 class Subcategory(models.Model):
+    """ Модель подкатегории """
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     title = models.CharField(max_length=50, verbose_name='Категория')
     slug = models.SlugField(verbose_name='Слаг', unique=True)
@@ -29,8 +40,10 @@ class Subcategory(models.Model):
 
 
 class Product(models.Model):
+    """ Модель товара """
     title = models.CharField(max_length=150, verbose_name='Название')
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, verbose_name='Подкатегория')
+    price = models.PositiveIntegerField(verbose_name="Цена")
 
     def __str__(self):
         return f'{self.title}'
@@ -41,6 +54,7 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
+    """ Модель изображения товара """
     SIZES = [
         ('small', 'Small'),
         ('medium', 'Medium'),
@@ -54,3 +68,30 @@ class ProductImage(models.Model):
         verbose_name = 'Фото товара'
         verbose_name_plural = 'Фото товара'
 
+
+class Cart(models.Model):
+    """ Модель корзины """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='покупатель')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создана')
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
+    def __str__(self):
+        return f"Cart for {self.user}"
+
+
+class CartItem(models.Model):
+    """ Модель позиции в корзине """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name='корзина')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='анализ')
+    count = models.PositiveIntegerField(verbose_name="Кол-во")
+
+    class Meta:
+        verbose_name = 'Позиция в корзине'
+        verbose_name_plural = 'Позиции в корзине'
+
+    @property
+    def position_cost(self) -> int:
+        return self.count * self.product.price
--- shop/serializers.py
+++ shop/serializers.py
@@ -1,1 +1,41 @@
 from rest_framework import serializers
+
+from shop.models import Product, Category, Subcategory, Cart, CartItem
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    '''Сериализатор для категории товаров'''
+    class Meta:
+        model = Category
+        fields = ('title', 'image')
+
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    '''Сериализатор для подкатегории товаров'''
+    class Meta:
+        model = Subcategory
+        fields = ('title', 'category', 'image')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    '''Сериализатор для товаров'''
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    '''Сериализатор для товаров'''
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    '''Сериализатор для товаров'''
+
+    class Meta:
+        model = CartItem
+        fields = '__all__'
