148f12cf76223fe51919ee24cf59ea4e6a3ed730
Olga Khaidoukova <khaidoukova@inbox.ru>
2024-08-14 13:59:08 +0500

    final commit

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,4 +1,3 @@
-from django.utils.text import slugify
 from rest_framework import serializers
 
 from shop.models import Product, Category, Subcategory, Cart, CartItem
@@ -20,23 +19,43 @@ class SubcategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     '''Сериализатор для товаров'''
-
+    subcategory_title = serializers.CharField(source='subcategory.title', read_only=True)
+    category_title = serializers.CharField(source='subcategory.category.title', read_only=True)
     class Meta:
         model = Product
-        fields = '__all__'
-
-
-class CartSerializer(serializers.ModelSerializer):
-    '''Сериализатор для товаров'''
-
-    class Meta:
-        model = Cart
-        fields = '__all__'
+        fields = ('id', 'title', 'subcategory', 'subcategory_title', 'category_title', 'price')
+        extra_kwargs = {
+            'subcategory': {'required': True}
+        }
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    '''Сериализатор для товаров'''
+    '''Сериализатор для позиции в корзине'''
+    product = ProductSerializer()
 
     class Meta:
         model = CartItem
-        fields = '__all__'
+        fields = ('id', 'product', 'count')
+
+
+class CartSerializer(serializers.ModelSerializer):
+    '''Сериализатор для корзины'''
+    cartitems = CartItemSerializer(many=True, source='cartitem_set', read_only=True)
+    total_cart_price = serializers.SerializerMethodField()
+    total_count = serializers.SerializerMethodField()
+
+    def get_total_cart_price(self, obj: Cart) -> int:
+        ''' Рассчитываю общую стоимость корзины '''
+        total = 0
+        for item in obj.cartitem_set.all():
+            total += item.product.price * item.count
+        return total
+
+    def get_total_count(self, obj: Cart) -> int:
+        ''' Рассчитываю общую стоимость корзины '''
+        return sum(item.count for item in obj.cartitem_set.all())
+
+    class Meta:
+        model = Cart
+        fields = ('id', 'user', 'cartitems', 'total_cart_price', 'total_count')
+
--- shop/urls.py
+++ shop/urls.py
@@ -1,15 +1,31 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from shop.apps import ShopConfig
-from shop.views import CategoryCreateAPIView, CategoryListAPIView, CategoryRetrieveAPIView, CategoryUpdateAPIView
+from shop.views import CategoryCreateAPIView, CategoryListAPIView,  \
+    SubcategoryCreateAPIView, SubcategoryListAPIView, CartViewSet, \
+    ProductViewSet
 
 app_name = ShopConfig.name
-
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='products')
+router.register(r'cart', CartViewSet, basename='cart')
 
 urlpatterns = [
+    # работа с категориями
     path('category/create/', CategoryCreateAPIView.as_view(), name='category_create'),
     path('category/', CategoryListAPIView.as_view(), name='category_list'),
-    path('category/<int:pk>/', CategoryRetrieveAPIView.as_view(), name='category_detail'),
-    path('category/update/<int:pk>/', CategoryUpdateAPIView.as_view(), name='category_update'),
 
+    # работа с подкатегорями
+    path('subcategory/create/', SubcategoryCreateAPIView.as_view(), name='category_create'),
+    path('subcategory/', SubcategoryListAPIView.as_view(), name='category_list'),
+
+    # работа с продуктами
+    path('', include(router.urls)),
+    # работа с корзиной
+    path('cart/', CartViewSet.as_view({'get': 'list'})),
+    path('cart/add/<int:product_id>/', CartViewSet.as_view({'post': 'add_product'})),
+    path('cart/remove/<int:product_id>/', CartViewSet.as_view({'delete': 'remove_product'})),
+    path('cart/clear/', CartViewSet.as_view({'delete': 'clear_cart'})),
 
 ]
